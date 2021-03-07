c0b70775a988d2f89029dbb511d01ef5b1cee22b
fenghong <fenghong007@hotmail.com>
2021-03-07 21:11:14 +0800

    测试产生居中的问题

--- src/views/dashboard/generalStat.vue
+++ src/views/dashboard/generalStat.vue
@@ -2,44 +2,44 @@
   <div class="app-container">
     <el-row>
       <el-col :span="24" class="card-box">
-          <div>
-            <el-form :model="queryParams" ref="queryForm" :inline="true">
-            <el-form-item label="消费类型" prop="consumeType">
-              <el-select
-                v-model="queryParams.consumeType"
-                placeholder="方式"
-                clearable
+        <div style="text-align: left;">
+          <el-form :model="queryParams" ref="queryForm" :inline="true">
+          <el-form-item label="消费类型" prop="consumeType">
+            <el-select
+              v-model="queryParams.consumeType"
+              placeholder="方式"
+              clearable
+              size="small"
+              style="width: 120px"
+            >
+              <el-option
+                v-for="dict in consumeTypeOptions"
+                :key="dict.id"
+                :label="dict.text"
+                :value="dict.id"
+              />
+            </el-select>
+          </el-form-item>
+            <el-form-item label="统计日期">
+              <el-date-picker
+                v-model="dateRange"
                 size="small"
-                style="width: 120px"
-              >
-                <el-option
-                  v-for="dict in consumeTypeOptions"
-                  :key="dict.id"
-                  :label="dict.text"
-                  :value="dict.id"
-                />
-              </el-select>
+                style="width: 240px"
+                value-format="yyyy-MM-dd"
+                type="daterange"
+                unlink-panels
+                range-separator="-"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                :picker-options="datePickerOptions"
+              ></el-date-picker>
             </el-form-item>
-              <el-form-item label="统计日期">
-                <el-date-picker
-                  v-model="dateRange"
-                  size="small"
-                  style="width: 240px"
-                  value-format="yyyy-MM-dd"
-                  type="daterange"
-                  unlink-panels
-                  range-separator="-"
-                  start-placeholder="开始日期"
-                  end-placeholder="结束日期"
-                  :picker-options="datePickerOptions"
-                ></el-date-picker>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="stat" icon="el-icon-s-data" size="mini" @click="handleQuery" v-hasPermi="['report:plan:planReport:stat']">统计</el-button>
-                <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
-              </el-form-item>
-            </el-form>
-          </div>
+            <el-form-item>
+              <el-button type="stat" icon="el-icon-s-data" size="mini" @click="handleQuery" v-hasPermi="['report:plan:planReport:stat']">统计</el-button>
+              <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
       </el-col>
 
       <el-col :span="16" class="card-box">
