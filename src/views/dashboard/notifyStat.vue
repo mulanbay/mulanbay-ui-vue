d3dc45beab84849155f1254abd49ed9def30697c
fenghong <fenghong007@hotmail.com>
2023-02-08 22:06:25 +0800

    完善页面显示

--- src/views/dashboard/notifyStat.vue
+++ src/views/dashboard/notifyStat.vue
@@ -6,46 +6,55 @@
       </div>
       <el-carousel v-loading="loading" height="295px">
         <el-carousel-item v-for="item in notifyData" :key="item.id">
-          <div class="el-table el-table--enable-row-hover el-table--medium" align="center">
-            <table cellspacing="0" style="width: 100%;">
-              <thead>
-                <tr>
-                  <th class="is-leaf"><div class="cell" align="center">字段</div></th>
-                  <th class="is-leaf"><div class="cell" align="center">数值</div></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td><div class="cell" align="center">提醒名称</div></td>
-                  <td><div class="cell" align="center">{{ item.userNotify.title }}</div></td>
-                </tr>
-                <tr>
-                  <td><div class="cell" align="center">统计值</div></td>
-                  <td>
-                    <div class="cell" align="center">
-                      <span v-if="item.overAlertValue>0" style="color:red ;">
-                       {{ item.content }}
-                      </span>
-                      <span v-else-if="item.overWarningValue>0" style="color:BlueViolet ;">
-                       {{ item.content }}
-                      </span>
-                      <span v-else style="color:#2E8B57 ;">
-                        {{ item.content }}
-                        <i class="el-icon-success" />
-                      </span>
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td><div class="cell" align="center">警告值</div></td>
-                  <td><div class="cell" align="center">{{ item.userNotify.warningValue+item.userNotify.unit }}</div></td>
-                </tr>
-                <tr>
-                  <td><div class="cell" align="center">报警值</div></td>
-                  <td><div class="cell" align="center">{{ item.userNotify.alertValue+item.userNotify.unit }}</div></td>
-                </tr>
-              </tbody>
-            </table>
+          <div>
+            <el-descriptions class="margin-top" title="提醒统计" :column="1" :size="size" border labelStyle="width: 100px">
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-info"></i>
+                  提醒名称
+                </template>
+                <div class="cell">{{ item.userNotify.title }}</div>
+              </el-descriptions-item>
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-s-data"></i>
+                  统计值
+                </template>
+                <div class="cell">
+                  <span v-if="item.overAlertValue>0" style="color:red ;">
+                   {{ item.content }}
+                  </span>
+                  <span v-else-if="item.overWarningValue>0" style="color:BlueViolet ;">
+                   {{ item.content }}
+                  </span>
+                  <span v-else style="color:#2E8B57 ;">
+                    {{ item.content }}
+                    <i class="el-icon-success" />
+                  </span>
+                </div>
+              </el-descriptions-item>
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-warning"></i>
+                  警告值
+                </template>
+                <div class="cell">{{ item.userNotify.warningValue+item.userNotify.unit }}</div>
+              </el-descriptions-item>
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-star-on"></i>
+                  报警值
+                </template>
+                <div class="cell">{{ item.userNotify.alertValue+item.userNotify.unit }}</div>
+              </el-descriptions-item>
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-d-caret"></i>
+                  比较类型
+                </template>
+                <div class="cell">{{ item.userNotify.notifyConfig.notifyTypeName }}</div>
+              </el-descriptions-item>
+            </el-descriptions>
           </div>
         </el-carousel-item>
       </el-carousel>
